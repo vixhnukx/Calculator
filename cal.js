1b8a53e148fd05e897438115205119d96170327b
unknown <vixhnukx@gmail.com>
2023-01-05 22:27:33 +0530

    calculator

---
+++ cal.js
@@ -0,0 +1,19 @@
+function display(num){
+    result.value+=num
+}
+
+function allclear(){
+    result.value=""
+}
+
+function evaluateexp(){
+    // currentExp=result.value
+    // result.value=eval(currentExp)
+    result.value=eval(result.value)
+}
+function backspace(){
+    // data=result.values=data.slice(0,-1)
+    // result.value=s 
+
+    result.value=result.value.slice(0,-1)
+}
